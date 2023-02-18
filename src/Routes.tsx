bb69a347d4b2496842c5986662873c71411c82ec
Ryan Kirsch <agalloch88@gmail.com>
2023-02-18 16:13:44 -0500

    set up Routes.tsx

---
+++ src/Routes.tsx
@@ -0,0 +1,36 @@
+import { Route, Link, Routes } from "react-router-dom";
+import { AuthEventData, AmplifyUser } from "@aws-amplify/ui";
+import { Home } from "./views/Home";
+import { CreateBoard } from "./views/CreateBoard";
+import { ViewBoard } from "./views/Board";
+import { ViewBoards } from "./views/ViewBoards";
+
+interface Props extends React.PropsWithChildren {
+  user: AmplifyUser | undefined;
+  signOut: ((data?: AuthEventData | undefined) => void) | undefined;
+}
+
+function AppRoutes(props: Props) {
+  return (
+    <div>
+      <div className="nav">
+        <Link to="/">
+          <button>Home</button>
+        </Link>
+
+        <h1>Idea Voting App</h1>
+
+        <button onClick={props.signOut}>Sign Out</button>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/boards" element={<ViewBoards />} />
+        <Route path="/boards/:id" element={<ViewBoard />} />
+
+        <Route path="/createboard/" element={<CreateBoard />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default AppRoutes;
