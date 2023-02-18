7604fdd53237b9100dec368333ffd7c8966a8328
Ryan Kirsch <agalloch88@gmail.com>
2023-02-18 16:03:32 -0500

    refactor App.tsx, add user to Authenticator

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
@@ -12,22 +10,11 @@ function App() {
   return (
     <div className="App">
       <Authenticator>
-        {({ signOut }) => (
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.tsx</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <button onClick={signOut}>Log Out</button>
-          </header>
+        {({ signOut, user }) => (
+          <main>
+            <AppRoutes user={user} signOut={signOut} />
+            <ToastContainer />
+          </main>
         )}
       </Authenticator>
     </div>
