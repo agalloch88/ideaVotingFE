89623842d9b57f655c227de8fe2f822dce1aff75
Ryan Kirsch <agalloch88@gmail.com>
2023-02-18 15:38:03 -0500

    wrap app with Authenticator and add logout button

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Amplify } from 'aws-amplify';
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import amplifyConfig from './aws-exports';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { Authenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
+import amplifyConfig from "./aws-exports";
 
 Amplify.configure(amplifyConfig);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Authenticator>
+        {({ signOut }) => (
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+            <button onClick={signOut}>Log Out</button>
+          </header>
+        )}
+      </Authenticator>
     </div>
   );
 }
