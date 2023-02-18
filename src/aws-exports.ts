0439b336f9b47ea6d1aabf0ea24fb97d5be83979
Ryan Kirsch <agalloch88@gmail.com>
2023-02-18 15:38:26 -0500

    write awsConfig for Cognito

---
+++ src/aws-exports.ts
@@ -0,0 +1,8 @@
+const awsConfig = {
+    aws_project_region: 'us-east-1',
+    aws_cognito_region: 'us-east-1',
+    aws_user_pools_id: 'us-east-1_A3Q4wWZTq',
+    aws_user_pools_web_client_id: '2jbfk1msjr8m63gjvssmdmrmij',
+};
+
+export default awsConfig;
